99aa7ea3bdcd462c36a35c7c3fe37afba94b4e47
Danialfarhan242 <150222670+Danialfarhan242@users.noreply.github.com>
2023-11-08 15:21:35 +0800

    Update settings.js
    Done

--- settings.js
+++ settings.js
@@ -11,17 +11,17 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'mLhc9UJO3F6t4KL', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
-global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.BOT_TOKEN = "6530481178:AAFsuZbaOgOkSVSS4P90xjh7SN5lMI7M1WQ" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "BABU NIEL" //your bot name
+global.OWNER_NAME = "𝙉𝙄𝙀𝙇𝙇`ᴸᴺᴷᴰ" //your name
+global.OWNER_NUMBER = "+6281918381454" //your telegram number
+global.OWNER = ["https://t.me/danialfarhan242", "https://t.me/danialfarhan242"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/6210760930833316188_121.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
